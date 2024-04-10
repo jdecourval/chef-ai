a92a332d742ba55f6bb706cce6788bce4430c5ed
Jerome <decje9@gmail.com>
2024-04-10 19:06:50 -0400

    Remove query.py

--- query.py
+++
@@ -1,5 +0,0 @@
-# todo: Try something like:
-# - Compute embedding on the user question
-# - Fetch articles until the context size is full, or until the match distance becomes too low. Order by match distance.
-# - Enrich the prompt with the articles.
-# This may not necessarily work better, experiment.
